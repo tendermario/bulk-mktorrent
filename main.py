2f558c5d3241198c8597f3f58afb1ed0725e6c63
Mario Viens <marviens@gmail.com>
2023-01-26 14:09:34 -0800

    Fix some bizarre method location changes, since 3 years since touching this. Also adding preflight check for mktorrent

--- main.py
+++ main.py
@@ -11,13 +11,20 @@ Options:
 """
 
 import os
+from os import path
 from dotenv import load_dotenv
 from docopt import docopt
+from typing import List
 
 
 def get_dirs(root: str) -> List[str]:
     """Returns a list of sub-directories from a root directory (non-recursively)."""
-    return [os.join(root, i) for i in os.listdir(root) if os.isdir(i)]
+    return [path.join(root, i) for i in os.listdir(root) if path.isdir(i)]
+
+def preflight_check():
+    myCmd = f'mktorrent'
+    if os.system(myCmd) != 0:
+        exit("mktorrent needs to be installed")
 
 def make_torrent(path, ANNOUNCE_URL):
     print(f'making a torrent for {path}')
@@ -35,6 +42,7 @@ if __name__ == "__main__":
         raise "Need an announce url. Set this in a .env file."
 
     dirs = get_dirs(rootdir)
+    preflight_check()
 
     for directory in dirs:
         make_torrent(directory, ANNOUNCE_URL)
