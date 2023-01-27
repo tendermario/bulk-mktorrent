27f364d09ca743f7a848c11fe52be32b8f6349db
Mario Viens <marviens@gmail.com>
2023-01-26 16:08:16 -0800

    Mute checking for mktorrents existence, switch to/from to be dynamic and fully from env file. Make folder verification better.

--- main.py
+++ main.py
@@ -5,10 +5,13 @@
 """
 
 Usage:
-    main.py <rootdir>
+    main.py
 
-Options:
-    -f --folder=<name>    Folder to make torrents from.
+Options in .env file:
+    FROM=          Folder of folders/files to each turn into torrents.
+    TO=            Folder to make .torrent files into. (Use full path)
+    ANNOUNCE_URL=  YOUR announce URL.
+    TRACKER=       The tracker flag. Orpheus is OPS, Redacted is RED.
 
 """
 
@@ -21,10 +24,10 @@ from typing import List
 
 def get_dirs(root: str) -> List[str]:
     """Returns a list of sub-directories from a root directory (non-recursively)."""
-    return [path.join(root, i) for i in os.listdir(root) if path.isdir(i)]
+    return [path.join(root, folder) for folder in os.listdir(root) if path.isdir(root + "/" + folder)]
 
 def preflight_check():
-    myCmd = f'mktorrent -h'
+    myCmd = f'mktorrent -h > /dev/null'
     if os.system(myCmd) != 0:
         prompt_install()
 
@@ -41,23 +44,32 @@ def install_mktorrent():
     os.system("git clone git@github.com:pobrn/mktorrent.git")
     os.system("cd mktorrent; make; sudo make install; cd ..; rm -rf mktorrent")
 
-def make_torrent(path, ANNOUNCE_URL):
-    print(f'making a torrent for {path}')
-    myCmd = f'mktorrent -l 18 -p -s OPS -a {ANNOUNCE_URL} "{path}"'
+def make_torrent(TO, TRACKER, ANNOUNCE_URL, PATH):
+    print(f'making a torrent for {PATH}')
+    myCmd = f'cd "{TO}"; mktorrent -l 18 -p -s {TRACKER} -a {ANNOUNCE_URL} "{PATH}"'
+    print(myCmd)
     os.system(myCmd)
 
 if __name__ == "__main__":
     load_dotenv()
-    arguments = docopt(__doc__)
-
-    rootdir = arguments['<rootdir>']
     ANNOUNCE_URL = os.getenv("ANNOUNCE_URL")
+    TRACKER = os.getenv("TRACKER")
+    FROM = os.getenv("FROM")
+    TO = os.getenv("TO")
 
     if not ANNOUNCE_URL:
         raise "Need an announce url. Set this in a .env file."
 
-    dirs = get_dirs(rootdir)
+    """ Only handle directories at this time. Could tweak this to be conditional"""
+    dirs = get_dirs(FROM)
     preflight_check()
 
-    for directory in dirs:
-        make_torrent(directory, ANNOUNCE_URL)
+    print("Running bulk-mktorrent\n\n")
+
+    print(f'There are {len(dirs)} folders we can create torrents from.')
+
+    print(f'Creating a torrent for each of the files/folders in {FROM} to {TO}\n')
+    for PATH in dirs:
+        make_torrent(TO, TRACKER, ANNOUNCE_URL, PATH)
+
+
